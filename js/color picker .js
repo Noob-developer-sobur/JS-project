98c90bf51c0aed932e44cab89053306b2ab5a3a0
Noob-developer-sobur <soburkhan210@gmail.com>
2023-05-17 19:12:38 +0600

    js-project

---
+++ js/color picker .js
@@ -0,0 +1,8 @@
+function changeContent (gateValue){
+    let bg = document.querySelector('.bgcolor');
+    let selectText = document.querySelector('.card');
+    bg.style.background = gateValue.value;
+}
+function soburKhan() {
+    document.getElementById("demo").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur alias rem suscipit natus voluptatum molestiae. Necessitatibus eveniet quibusdam dolores? Reiciendis quasi rerum ea nihil aspernatur molestiae blanditiis excepturi natus? Ex illo temporibus dignissimos ratione quaerat quos neque, ad cumque consequuntur mollitia necessitatibus saepe quasi quidem obcaecati nobis magnam vel eos nemo molestiae corporis suscipit eius aut quas itaque? Placeat error, optio eveniet aspernatur iste qui molestiae eius earum fugiat in temporibus eaque quas, deserunt possimus nobis odio dolorem laudantium laboriosam repellat, eligendi ut at? Deleniti quos corrupti culpa voluptates. Ipsam quae ipsum ex molestias, culpa reiciendis obcaecati fuga natus quia!";
+}
